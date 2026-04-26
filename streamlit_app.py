28f319a4dc0d9fca3fc63e67a8a0bc3a40168d43
testuser3213478 <testuser3213478@gmail.com>
2026-04-26 14:56:28 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ if time_to_insert:
 
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
 
